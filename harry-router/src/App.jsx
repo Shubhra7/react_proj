4f0904b929866ede2490f887415326b7bb9bcc8c
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-17 13:33:53 +0530

    harry router video

---
+++ harry-router/src/App.jsx
@@ -0,0 +1,42 @@
+
+import './App.css'
+import Navbar from './Components/Navbar'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './Components/Home';
+import Login from './Components/Login';
+import About from './Components/About';
+import User from './Components/User';
+
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <><Navbar /><Home/ ></>
+    },
+    {
+      path:"/login",
+      element: <><Navbar /><Login /></>
+    },
+    {
+      path:"/about",
+      element: <><Navbar /><About /></>
+    },
+    {
+      path:"/user/:username",
+      element: <><Navbar /><User /></>
+    }
+  ])
+
+
+  return (
+    <>
+        
+        <RouterProvider router={router} />
+  
+    </>
+  )
+}
+
+export default App
