7ec8e77d95fc9de0ce858b0c2664202117603a6c
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-21 19:58:53 +0530

    custom hook with project currency convertor

---
+++ 06currencyconvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-3xl bg-orange-300 border-spacing-1' style={{borderRadius: '10px',
+    width:'90%'}}>Naddan pareindee</h1>
+    </>
+  )
+}
+
+export default App
