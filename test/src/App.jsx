98b4dd54276b0683fe1db3db8a2a4b72b7a06489
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-21 18:03:21 +0530

    test change

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -43,7 +43,7 @@ function App() {
     <div className="w-full max-w-md mx-auto shadow-md
       rounded-lg px-4 my-8 text-orange-500
        bg-gray-700">
-        <h1 className="text-white text-center my-3">Password Generator</h1>
+        <h1 className="text-white text-center my-3">Password Generator here</h1>
         <div className="flex shadow rounded-lg 
         overflow-hidden mb-6">
         <input 
