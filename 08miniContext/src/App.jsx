c2c8ffcc13e9a8884fe77160d4f099c63e042c2b
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-03 10:57:01 +0530

    feat: implement UserContext and UserContextProvider for user state management

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
 
   return (
     <>
+    <UserContextProvider>
       <h1>React with Bubai</h1>
+    </UserContextProvider>
     </>
   )
 }
---
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UserContext from "./UserContext";
+
+//  children is like the inner div or content when used to enclose the code
+const UserContextProvider = ({children}) =>{
+    const [user, setUser] = React.useState(null)
+    return (
+        <UserContext.Provider value={{user,setUser}}>
+            {children}  // like "oulet"
+        </UserContext.Provider>
+    )
+
+}
+
+export default UserContextProvider
