06c178ffc83dca6168e58d5b473a6fef14f13191
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-03 13:32:36 +0530

    feat: integrate ThemeBtn and Card components, enhance theme switching functionality
    and important addition in css for tailwind dark mode

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
 
 function App() {
   const [themeMode,setThemeMode] = useState("light");
@@ -26,10 +28,13 @@ function App() {
               <div className="w-full">
                   <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
                       {/* ThemeBtn */}
+                      <ThemeBtn/>
+                      
                   </div>
 
                   <div className="w-full max-w-sm mx-auto">
                       {/* Card */}
+                      <Card/>
                   </div>
               </div>
           </div>
