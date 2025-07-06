acf4af51038ade6a921006b1e736b47534d18b0f
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-06 16:39:31 +0530

    feat: integrate Tailwind CSS and implement authentication with Appwrite
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated App component to manage user authentication state using Redux.
    - Created authSlice for managing authentication state with login and logout actions.
    - Implemented Footer and Header components for layout.
    - Refactored authService to correctly set the Appwrite project.
    - Updated main entry point to include Redux Provider.
    - Replaced existing CSS with Tailwind CSS imports for styling.

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,13 +1,40 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import { Footer, Header } from './components';
 
-function App() {
- console.log(import.meta.env.VITE_APPWRITE_URL);
- 
+function App() { 
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Mega Blog app with Appwrite</h1>
-    </>
+  useEffect(()=>{
+    // Take appwrite service (getCurrentUser)
+    // Then use my redux authSlice for dispatch the login() to store in "store"
+    authService.getCurrentUser()
+          .then((userData) => {
+            if(userData){
+              dispatch(login({userData}))
+            } else {
+              dispatch(logout())
+            }
+          })
+          .finally(()=> setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO: {/* <Outlet /> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (
+    <div> Loading..... </div>
   )
 }
 
