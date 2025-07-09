0c84981332c172a4ccb9e8d1dca8bfa41af085a2
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-09 17:26:56 +0530

    feat: add Signup and Login pages, and AllPosts page with post fetching and display

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -7,6 +7,11 @@ import Button from "./Button";
 import Input from "./Input";
 import Select from './Select'
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+
 export {
     Header,
     Footer,
@@ -16,5 +21,9 @@ export {
     Button,
     Input,
     Select,
-    RTE
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard
 }
