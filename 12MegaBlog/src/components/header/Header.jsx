f72d05bd06259f2fc1edec6229a3005af9004bf8
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-10 11:54:44 +0530

    fix: correct auth status selector in Header component

--- 12MegaBlog/src/components/header/Header.jsx
+++ 12MegaBlog/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-  const authStatus = useSelector((state) => state.auth.status)
+  const authStatus = useSelector((state) => <state className="auth"></state>.status)
   const navigate = useNavigate()
 
 //  slug === URL [just named as slug]
--- 12MegaBlog/src/pages/Login.jsx
+++ 12MegaBlog/src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Login as loginComponent } from '../components'
 
 const Login = () => {
+  console.log("Hi from login page");
+  
   return (
     <div className='py-8'>
       <loginComponent />
